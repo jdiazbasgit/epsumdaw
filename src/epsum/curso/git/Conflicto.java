3d54e207341529e579e635c14e00090231028319
Luis Rogelio Toledo <luis.toledo.sanblas.estudiante@gmail.com>
2024-02-29 13:56:58 +0100

    comentario de Luis

--- src/epsum/curso/git/Conflicto.java
+++ src/epsum/curso/git/Conflicto.java
@@ -1,10 +1,7 @@
 package epsum.curso.git;
 
 public class Conflicto {
-<<<<<<< HEAD
     // Soy Luis
-=======
-//Soy Sekan
-//soy profe 
->>>>>>> 4b6deaac8e7607c0b4d25fe93f5ea9d1f9f385df
+    // Soy Sekan
+    // soy profe
 }
