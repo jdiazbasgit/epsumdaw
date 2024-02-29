bc1281d5ef3b97f732dc7289dc2038ce24bd37a5
David Rodríguez de Lope López <149677468+drodriguez5503@users.noreply.github.com>
2024-02-29 13:48:00 +0100

    Tercera version

--- src/epsum/curso/git/Conflicto.java
+++ src/epsum/curso/git/Conflicto.java
@@ -2,13 +2,7 @@ package epsum.curso.git;
 
 public class Conflicto {
     public static void main(String[] args){
-        int p=3;
-        double f=5;
-        System.out.println(f);
-
-        for(int i=0; i<5;i++){
-            System.out.println(p);
-        }
+        //David 
     }
 
 }
