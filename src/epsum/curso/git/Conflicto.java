c95771ccf5283c59b256736b55ef0432cd6dc3ab
Your Name <you@example.com>
2024-03-01 09:04:32 +0100

    cambio

--- src/epsum/curso/git/Conflicto.java
+++ src/epsum/curso/git/Conflicto.java
@@ -3,20 +3,12 @@ package epsum.curso.git;
 
 
 public class Conflicto {
-//soy marcos
-//Soy Nour 
-// Soy Luis
-//Soy Sekan
-//soy profe 
-//soy danieliiitaaa
-// Soy Martin
+
     
     }
 
 public class Conflicto {
-// <3 Irene <3
-//Soy Sekan
-//soy profe 
+
 //Michael
     //David 
 
