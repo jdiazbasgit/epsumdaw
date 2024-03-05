0c550451a4088b77e8bff0d34bd6b84cc27f4aed
Your Name <you@example.com>
2024-03-05 12:41:59 +0100

    up

--- src/epsum/daw/basedatos/BaseDatos.java
+++ src/epsum/daw/basedatos/BaseDatos.java
@@ -10,33 +10,38 @@ import java.sql.Statement;
 public class BaseDatos {
 
 	public static void main(String[] args) {
-		 Connection conexion1=null;
+		Connection conexion = null;
 		// try {
-		try (Connection conexion = DriverManager.getConnection(
-				"jdbc:mysql://192.168.1.71:3306/empresas?useSSL=false&serverTimezone=UTC", "curso", "Cursocurso1;");) {
-			conexion1=conexion;
+		try {
+			conexion = DriverManager.getConnection(
+					"jdbc:mysql://192.168.1.71:3306/empresas?useSSL=false&serverTimezone=UTC", "curso", "Cursocurso1;");
+			conexion.setAutoCommit(false);
+			//conexion1 = conexion;
 			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
 			// Class.forName("com.mysql.cj.jdbc.Driver");
-			// conexion=DriverManager.getConnection("jdbc:mysql://192.168.1.71:3306/empresas?useSSL=false&serverTimezone=UTC",
+			// conexion=DriverManager.getConnection("jdbc:mysql://192.178.1.71:3306/empresas?useSSL=false&serverTimezone=UTC",
 			// "curso", "Cursocurso1;");
-			Statement statement = conexion1.createStatement();
-			int resultado = statement.executeUpdate("insert into cargos (descripcion) values ('Directora1')");
+
+			conexion.setAutoCommit(false);
+			Statement statement = conexion.createStatement();
+			int resultado = statement.executeUpdate("insert into cargos (descripcion) values ('Directora17')");
 			System.out.println(resultado);
 			System.out.println("conexion creada correctamente");
 
-			PreparedStatement preparedStatement = conexion1
+			PreparedStatement preparedStatement = conexion
 					.prepareStatement("insert into cargos (descripcion) values(?)");
-			String[] cargos = { "programador111", "limpiador111", "conductor111", "jefe de proyecto111" };
+			String[] cargos = { "programador1117", "limpiador1117", "conductor1117", "jefe de proyecto1117" };
 			for (int i = 0; i < cargos.length; i++) {
 				preparedStatement.setString(1, cargos[i]);
 				preparedStatement.execute();
 
 			}
-
+			conexion.commit();
 			ResultSet resultSet = statement.executeQuery("select id,descripcion from cargos");
 			while (resultSet.next()) {
 				System.out.println(resultSet.getInt(1) + " - " + resultSet.getString(2));
 			}
+			conexion.close();
 
 		} catch (
 
@@ -44,13 +49,14 @@ public class BaseDatos {
 			e.printStackTrace();
 			System.out.println("conexion no creada");
 		}
+
 		
-		try {
+		/*try {
 			System.out.println(conexion1.isClosed());
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
 	}
 	/*
 	 * finally {
@@ -60,8 +66,7 @@ public class BaseDatos {
 	 * 
 	 * 
 	 * 
-	 * }*/
-	 
-	 
+	 * }
+	 */
 
 }
