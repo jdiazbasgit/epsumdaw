449efe4b3d9e69dc9e91b59814b20d456e5ef476
antoniocc2005 <outside2005@gmail.com>
2024-03-07 13:33:39 +0100

    antonio

--- src/epsum/curso/git/Conflicto.java
+++ src/epsum/curso/git/Conflicto.java
@@ -2,23 +2,24 @@ package epsum.curso.git;
 
 public class Conflicto {
 //Michael
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
     //David 
-<<<<<<< HEAD
+
 	//Luis
 =======
 	//profe
 	// <3 Irene <3
 =======
-//David 
-//profe
-// <3 Irene <3
-//Luis (2)
-=======
+ 
+
+
+
+
 //David Rodríguez de lope López
-//profe
-// <3 Irene <3
+
+
 // Martin
->>>>>>> 4131f3207053fa24c79e62cd0b1481768d5fc1bc
+// antonio
+
 }
