f3af5f527671e78b21d65b4cfba3aa43b200ef56
Your Name <you@example.com>
2024-03-08 10:27:29 +0100

    Sara7

---
+++ src/epsum/daw/basedatos/entidades/Empresa.java
@@ -0,0 +1,19 @@
+package epsum.daw.basedatos.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Empresas")
+public class Empresa {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String nombre;
+	private String cif;
+	
+	
+}
