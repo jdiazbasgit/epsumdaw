8e11af6d9960abfee682b10338ca2038d9823d74
David Rodríguez de Lope López <149677468+drodriguez5503@users.noreply.github.com>
2024-03-08 09:41:35 +0100

    Entidades Cargos,Estados Civiles, Empresa - david

---
+++ src/epsum/daw/basedatos/entidades/Empresa.java
@@ -0,0 +1,22 @@
+package epsum.daw.basedatos.entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "empresa")
+public class Empresa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String nombre;
+	
+	private String cif;
+
+}
