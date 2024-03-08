1e37ceb88649bcb56e33c7fbd4580370061d7613
ireneClases <irene.epsum@gmail.com>
2024-03-08 10:36:19 +0100

    Nuevas clases, hijo, cargo, estado civil, empresa 080324 clase

---
+++ src/epsum/daw/basedatos/entidades/Empresas.java
@@ -0,0 +1,23 @@
+package epsum.daw.basedatos.entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "empresa")
+public class Empresas {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+		
+	@Column(length = 45)
+	private String nombre;
+	
+	@Column(length = 9)
+	private String cif;
+
+}
