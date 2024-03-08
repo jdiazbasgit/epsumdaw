973967e2e49ef417385416dc3e0b43b8011d4942
Luis Rogelio Toledo <luis.toledo.sanblas.estudiante@gmail.com>
2024-03-08 10:53:15 +0100

    se modifico elñ nombre de las tablas en las clases a minusculas

--- src/epsum/daw/basedatos/entidades/Cargo.java
+++ src/epsum/daw/basedatos/entidades/Cargo.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Cargos")
+@Table(name = "cargos")
 public class Cargo {
 
 	@Id
--- src/epsum/daw/basedatos/entidades/Empresa.java
+++ src/epsum/daw/basedatos/entidades/Empresa.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Empresas")
+@Table(name = "empresas")
 public class Empresa {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
