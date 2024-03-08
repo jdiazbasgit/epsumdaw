bcd19c9501c002f9f70edfffe25705ea8c161af2
Your Name <you@example.com>
2024-03-08 10:16:24 +0100

    sekan

---
+++ src/epsum/daw/basedatos/entidades/Cargo.java
@@ -0,0 +1,19 @@
+package epsum.daw.basedatos.entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cargos")
+public class Cargo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String descripcion;
+
+}
---
+++ src/epsum/daw/basedatos/entidades/EstadoCivil.java
@@ -0,0 +1,19 @@
+package epsum.daw.basedatos.entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "estados-civiles")
+public class EstadoCivil {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String descripcion;
+	
+}
