5edf8dbf95aebcbde90fa6be9637efe41cd77b10
Your Name <you@example.com>
2024-03-11 13:13:42 +0100

    a

---
+++ src/epsum/daw/basedatos/entidades/DatoLaboral.java
@@ -0,0 +1,29 @@
+package epsum.daw.basedatos.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity //soy una entidad
+@Table(name="datos_laborales")
+public class DatoLaboral {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private int id;
+	private int salario;
+	@ManyToOne
+	private Cargo cargo;
+	public DatoLaboral(int id, int salario, Cargo cargo) {
+		super();
+		this.id = id;
+		this.salario = salario;
+		this.cargo = cargo;
+	}
+
+	
+
+}
